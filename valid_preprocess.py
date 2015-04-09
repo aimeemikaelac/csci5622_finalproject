dba892d7e1c47f3dfd203210406ce24df85e4b2a
Michael Coughlin <michael.coughlin@colorado.edu>
2015-04-09 17:02:18 -0600

    fixed errors in valiation

--- valid_preprocess.py
+++ valid_preprocess.py
@@ -9,6 +9,7 @@ import argparse
 import csv
 import nltk
 import preprocess_data
+import sys
 
 
 if __name__ == "__main__":
@@ -24,40 +25,46 @@ if __name__ == "__main__":
                            default='questions_processed.csv', required=False)    
     args = argparser.parse_args()
     
+    if len(args.in_files) < 1:
+        print "No input files received. Exiting."
+        sys.exit()
     files = args.in_files.split(",")
-    if len(files) < 1:
-        print "No input files received. Exiting."    
-    processed_data = Dict()
-    missing_data = Dict()
+    processed_data = {}
+    missing_data = {}
     
     for in_file in files:
         infile = open(in_file)
-        reader = csv.DictReader(infile, ['id', 'answer', 'type', 'category', 'question', 'keywords'])
+        reader = csv.DictReader(infile)
         for row in reader:
+        #    print row['id']
             processed_data[int(row['id'])] = row
         infile.close()
     
     infile = open(args.question_file)
-    reader = csv.DictReader(infile, ['id', 'answer', 'type', 'category', 'question', 'keywords'])
+    reader = csv.DictReader(infile,['id', 'answer', 'type', 'category', 'question', 'keywords'])
     for row in reader:
         if int(row['id']) not in processed_data:
             missing_data[int(row['id'])] = row
-            
+    infile.close()
+         
+    print "Number of questions missed: "+ str(len(missing_data))
+
     tagger = preprocess_data.train_taggger()
-    for question_data in missing_data:
-        raw_question =  question_data['question']
-        prepared_question = preprocess_data.prepared_question(raw_question)
-        tagged_question = preprocess_data.tag_question(tagger, prepared_question, question_data['id'])
-        processed_data[int(question_data['id'])] = {'id':question_data['id'], 'answer':question_data['answer'],
-                                                    'type':question_data['type'],'category':question_data['category'],
-                                                    'question':tagged_question, 'keywords':question_data['keywords']}
+    for question_id in missing_data:
+        raw_question =  missing_data[question_id]['question']
+        prepared_question = preprocess_data.prepare_question(raw_question)
+        tagged_question = preprocess_data.tag_question(tagger, prepared_question, question_id)
+        processed_data[question_id] = {'id':missing_data[question_id]['id'], 'answer':missing_data[question_id]['answer'],
+                                                    'type':missing_data[question_id]['type'],'category':missing_data[question_id]['category'],
+                                                    'question':tagged_question, 'keywords':missing_data[question_id]['keywords']}
     
     out_file_name = args.out_file
     outfile = open(out_file_name, 'w')
     writer = csv.DictWriter(outfile, ['id', 'answer', 'type', 'category', 'question', 'keywords'])
     writer.writeheader()
-    for question_data in processed_data:
-        writer.writerow(question_data)
-    
+    for question_id in processed_data:
+        writer.writerow(processed_data[question_id])
+    outfile.close()
+
     print "Finished"
         
